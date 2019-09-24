fa8b166dd7479997f49eb45a2423b1a8385df424
MAIMEEARAI <pakaphon23@gmail.com>
2019-09-24 11:33:13 +0700

    LikeCount and CommentCount

--- server/graphql/Queries/posts.js
+++ server/graphql/Queries/posts.js
@@ -1,10 +1,48 @@
 const PostTC = require('../Typecompose/post')
+const Post = require('../Model/posts')
+
+PostTC.addResolver({
+    name:"LikeCount",
+    type:`type LikeInfo{
+        Like : Int
+    }`,
+    args:{
+        postId : 'String'
+    },
+    resolve : async ({args}) =>{
+        const { postId } = args
+        const post = await Post.findById({_id : postId})
+
+        const count = await post.Likes
+
+        return { Like : count.length }
+    }
+})
 
 
+PostTC.addResolver({
+    name:"CommentsCount",
+    type:`type CommnetInfo{
+        Comment : Int
+    }`,
+    args:{
+        postId : 'String'
+    },
+    resolve : async ({args}) =>{
+        const { postId } = args
+        const post = await Post.findById({_id : postId})
+
+        const count = await post.Likes
+
+        return { Comment : count.length }
+    }
+})
 
 const objpost = {
     postMany : PostTC.getResolver('findMany'),
-    postById : PostTC.getResolver('findById')
+    postById : PostTC.getResolver('findById'),
+    likeCount: PostTC.getResolver('LikeCount'),
+    commentCount : PostTC.getResolver('CommentsCount')
 }
 
 module.exports = objpost
